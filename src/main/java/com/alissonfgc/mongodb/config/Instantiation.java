3b747e0abbee01641f3bb36d7b2cba3971c7b7a2
alissonfgc <alissonfgc01@gmail.com>
2024-07-11 19:54:13 -0300

    Projection of author data with DTO

--- src/main/java/com/alissonfgc/mongodb/config/Instantiation.java
+++ src/main/java/com/alissonfgc/mongodb/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.alissonfgc.mongodb.domain.Post;
 import com.alissonfgc.mongodb.domain.User;
+import com.alissonfgc.mongodb.dto.AuthorDTO;
 import com.alissonfgc.mongodb.repository.PostRepository;
 import com.alissonfgc.mongodb.repository.UserRepository;
 
@@ -33,11 +34,12 @@ public class Instantiation implements CommandLineRunner {
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com");
 		User u3 = new User(null, "Bob Grey", "bob@gmail.com");
-
-		Post p1 = new Post(null, LocalDate.parse("21/03/2018", fmt), "Partiu viagem", "Vou viajar para São Paulo. Abraços", u1);
-		Post p2 = new Post(null, LocalDate.parse("23/03/2018", fmt), "Bom dia", "Acordei feliz hoje!", u1);
-
+		
 		userRepository.saveAll(Arrays.asList(u1, u2, u3));
+
+		Post p1 = new Post(null, LocalDate.parse("21/03/2018", fmt), "Partiu viagem", "Vou viajar para São Paulo. Abraços", new AuthorDTO(u1));
+		Post p2 = new Post(null, LocalDate.parse("23/03/2018", fmt), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(u1));
+
 		postRepository.saveAll(Arrays.asList(p1, p2));
 	}
 }
